b98d91578566e713efc5d20a471b1470268c22e3
grld <grldfaure@gmail.com>
2020-01-13 16:18:20 +0100

    coucou

--- connexion.php
+++ connexion.php
@@ -1,3 +1,60 @@
+<?php
+    include('includes/config.php');
+    if(isset($_POST['formConnect'])){
+        if(!empty($_POST['userNameConnect']) && !empty($_POST['passWordConnect'])){
+            $userNameConnect = $_POST['userNameConnect'];
+            $passWordConnect = sha256($_POST['passwordConnect']);
+            $search = $db->query('SELECT * FROM users WHERE username = "'.$userNameConnect.'"');
+            $userExist = $search->rowCount();
+            if($userExist == 1){
+                $user = $search->fecth(PDO::FETCH_ASSOC);
+                if($user['password'] == $passWordConnect){
+                    $_SESSION['id'] == $user['id'];
+                    header('Location: index.php');
+                } else {
+                    $error = "Identifiants invalides.";
+                }
+            } else {
+                $error = "Ce compte n'existe pas.";
+            }
+        } else {
+            $error = "Vous devez remplir tout le formulaire";
+        }
+    }
+
+    if(isset($_POST['formRegister'])){
+        if(!empty($_POST['userNameRegister']) && !empty($_POST['emailRegister']) && !empty($_POST['passWordRegister1']) && !empty($_POST['passWordRegister2'])){
+            $userNameRegister = $_POST['userNameRegister'];
+            $emailRegister = $_POST['emailRegister'];
+            $passWordRegister1 = $_POST['passWordRegister1'];
+            $passWordRegister2 = $_POST['passWordRegister2'];
+            $searchMail = $db->query('SELECT * FROM users WHERE mail = "'.$emailRegister.'"');
+            $mailExist = $searchMail->rowCount();
+            if($mailExist == 0){
+                $searchUserName = $db->query('SELECT * FROM users WHERE username = "'.$userNameRegister.'"');
+                $userNameExist = $searchUserName->rowCount();
+                if($userNameExist == 0){
+                    if($passWordRegister1 == $passWordRegister2){
+                        $addUser = $db->prepare('INSERT INTO users(mail, username, password) VALUES(:mail, :username, :password)');
+                        $addUser->execute(array(
+                            'mail' => $emailRegister,
+                            'username' => $userNameRegister,
+                            'password' => sha256($passWordRegister1)
+                        ));
+                        header('Location: index.php');
+                    } else {
+                        $error = "Les mots de passes ne correspondent pas !";
+                    }
+                } else {
+                    $error = "Ce nom d'utilisateur est déjà utilisé !";
+                }
+            } else {
+                $error = "Cette adresse email est déjà utilisée.";
+            }
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html>
     
@@ -10,11 +67,17 @@
          
     </head>
     <body>
+<<<<<<< HEAD
+        <div class="main">
+=======
+        <?php
+            if(!isset($_GET['registred'])){
+        ?>
+>>>>>>> c7e92d9d13b88e7d8be95e68b15cdb7457735a89
         <div class="container">
-            <h1>Connexion</h1>
                 <form action="" method="get" class="connexion">
                     <div class="formConnexion">
-                        <label for="userNameConnect">Entrez votre nom: </label>
+                        <label for="userNameConnect">Entrez votre pseudo: </label>
                             <input type="text" name="userNameConnect" id="userNameConnect" required>
                     </div>
                     <div class="formPassWord">
@@ -27,9 +90,10 @@
                 </form>
             </div>
             <div class="form-example">
+<<<<<<< HEAD
                        <a href="#">Inscrivez-vous!</a>
                     </div>
-            <div class="container">
+            <div class="container" id="container2">
             <h1>Inscription</h1>
                 <form action="" method="get" class="connexion">
                     <div class="formConnexion">
@@ -53,4 +117,39 @@
                     </div>
                 </form>
             </div>
+            </div>
+=======
+                <a href="#">Inscrivez-vous!</a>
+            </div>
+        <?php
+            } else {
+        ?>
+            <div class="container">
+            <h1>Inscription</h1>
+            <form action="" method="POST" class="connexion">
+                <div class="formConnexion">
+                    <label for="userNameConnect">Entrez votre nom: </label>
+                        <input type="text" name="userNameConnect" id="userNameConnect" required>
+                </div>
+                <div class="formConnexion">
+                    <label for="email">Entrez votre email: </label>
+                        <input type="email" name="emailRegister" id="email" required>
+                </div>
+                <div class="formPassWord1">
+                    <label for="passWord1">Entrez votre mot de passe :</label>
+                    <input type="password" name="passWordRegister1" id="passWord1" required>
+                </div>
+                <div class="formPassWord2">
+                    <label for="passWord2">Vérifiez votre mot de passe :</label>
+                    <input type="password" name="passWordRegister2" id="passWord2" required>
+                </div>
+                <div class="form-example">
+                    <input type="submit" value="Envoyer" name="formRegister">
+                </div>
+            </form>
+        </div>
+        <?php
+            }
+        ?>
+>>>>>>> c7e92d9d13b88e7d8be95e68b15cdb7457735a89
     </body>
