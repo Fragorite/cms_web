bdc7476a1996bb63463aac8bf04044c6d2b53aa9
Fragorite <earvin.mars@gmail.com>
2020-01-14 14:27:15 +0100

    dzqdqzdq

--- administration/includes/menu.php
+++ administration/includes/menu.php
@@ -2,8 +2,8 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <title>Connexion</title>
-<link rel="stylesheet" href="../css/connexion.css">
-<link rel="stylesheet" href="../css/menu.css">
+<link rel="stylesheet" href="../../css/connexion.css">
+<link rel="stylesheet" href="../../css/menu.css">
 <link href="https://fonts.googleapis.com/css?family=Raleway:400,600&display=swap" rel="stylesheet"> 
 
 
