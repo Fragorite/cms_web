7cb37b88b10abca0f2e3e90bc635574e6aa88401
grld <grldfaure@gmail.com>
2020-01-14 16:14:29 +0100

    menuflex

--- includes/header.php
+++ includes/header.php
@@ -2,15 +2,17 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <title>Connexion</title>
 <meta name="description" content="Page de connexion au super mortel CMS.">
-<link rel="stylesheet" href="css/connexion.css">
-<link rel="stylesheet" href="css/header.css">
+<link rel="stylesheet" href="../css/connexion.css">
+<link rel="stylesheet" href="../css/header.css">
 <link href="https://fonts.googleapis.com/css?family=Raleway:400,600&display=swap" rel="stylesheet"> 
 
-<nav class="navigation">
-    <ul>
+<div>
+    <ul class="navigationHead">
+        <li><a href="#">Ici le Titre</a></li>
         <li><a href="#">Articles</a></li>
         <li><a href="#">Forum</a></li>
         <li><a href="#">Tchat</a></li>
         <li><a href="#">Administration</a></li>
+        <li><a href="#">Déconnexion</a></li>
     </ul>
-</nav>
+</div>
