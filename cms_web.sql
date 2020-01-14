496907ffc302dbaa9bc61dff2dac2ba8b2bb94a9
Fragorite <earvin.mars@gmail.com>
2020-01-14 14:36:21 +0100

    Update cms_web.sql

--- cms_web.sql
+++ cms_web.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  lun. 13 jan. 2020 à 16:31
+-- Généré le :  mar. 14 jan. 2020 à 14:35
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.3.12
 
@@ -25,6 +25,40 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `frm_answers`
+--
+
+CREATE TABLE `frm_answers` (
+  `id` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_subject` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `frm_subjects`
+--
+
+CREATE TABLE `frm_subjects` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `date_publication` varchar(255) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `frm_subjects`
+--
+
+INSERT INTO `frm_subjects` (`id`, `title`, `content`, `date_publication`, `id_user`) VALUES
+(1, 'Premier pas', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic, nobis tenetur eveniet aspernatur earum culpa fugiat expedita doloribus ipsam, excepturi dolore dolorum itaque, eaque reprehenderit doloremque provident unde adipisci? Minus.Lorem, ipsum dolor ', '01/01/2020 à 00:00', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `users`
 --
 
@@ -42,9 +76,40 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `mail`, `username`, `password`) VALUES
 (1, 'TEST@TEST.TEST', 'tz', '098f6bcd4621d373cade4e832627b4f6');
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `website`
+--
+
+CREATE TABLE `website` (
+  `websiteName` varchar(255) NOT NULL,
+  `template` int(11) NOT NULL,
+  `init` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `website`
+--
+
+INSERT INTO `website` (`websiteName`, `template`, `init`) VALUES
+('TESTTEST', 2, 1);
+
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `frm_answers`
+--
+ALTER TABLE `frm_answers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `frm_subjects`
+--
+ALTER TABLE `frm_subjects`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `users`
@@ -55,6 +120,18 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT pour les tables déchargées
 --
+
+--
+-- AUTO_INCREMENT pour la table `frm_answers`
+--
+ALTER TABLE `frm_answers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `frm_subjects`
+--
+ALTER TABLE `frm_subjects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `users`
