df6d5c76ef2d926b617314579ae775470381f8b2
grld <grldfaure@gmail.com>
2020-01-14 13:55:11 +0100

    coucou

--- administration/includes/menu.php
+++ administration/includes/menu.php
@@ -1,3 +1,13 @@
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>Connexion</title>
+<link rel="stylesheet" href="css/connexion.css">
+<link rel="stylesheet" href="css/menu.css">
+<link href="https://fonts.googleapis.com/css?family=Raleway:400,600&display=swap" rel="stylesheet"> 
+
+
+
 <nav class="navigation">
   <ul class="mainmenu">
     <li><a href="">Article</a></li>
---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>Connexion</title>
+<meta name="description" content="Page de connexion au super mortel CMS.">
+<link rel="stylesheet" href="css/connexion.css">
+<link href="https://fonts.googleapis.com/css?family=Raleway:400,600&display=swap" rel="stylesheet"> 
+
+
