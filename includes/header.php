7484165ec138f2346e1b2bc971b7ad66bc5c3ad7
grld <grldfaure@gmail.com>
2020-01-15 13:33:14 +0100

    gh

--- includes/header.php
+++ includes/header.php
@@ -3,8 +3,9 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="Page de connexion au super mortel CMS.">
-<link rel="stylesheet" href="css/header.css">
+
 <link rel="stylesheet" href="css/connexion.css">
+<link rel="stylesheet" href="css/header.css">
 <link href="https://fonts.googleapis.com/css?family=Raleway:400,600&display=swap" rel="stylesheet"> 
 <?php
         if($infosSite['template'] == 1) {
