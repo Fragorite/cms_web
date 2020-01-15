5362680cabc38ebf5a584d83c942adb63cb0056f
grld <grldfaure@gmail.com>
2020-01-15 11:49:56 +0100

    dfd

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+<?php include('includes/config.php'); ?>
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Page de connexion au super mortel CMS.">
+<link rel="stylesheet" href="css/header.css">
+<link rel="stylesheet" href="css/connexion.css">
+<link href="https://fonts.googleapis.com/css?family=Raleway:400,600&display=swap" rel="stylesheet"> 
+
+<div>
+    <ul class="navigationFooter">
+        <li><a href="#">Copyright</a></li>
+        <li><a href="#">RGPD</a></li>
+        <li><a href="#">Coditions d'utilisations</a></li>
+        <li><a href="#">FB</a></li>
+        <li><a href="#">Twitter</a></li>
+        <li><a href="#">Linkedin</a></li>
+    </ul>
+</div>
