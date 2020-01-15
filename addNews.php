f4a589b52eb88b287e7d7e305a6deddfff944d88
Fragorite <earvin.mars@gmail.com>
2020-01-15 15:08:46 +0100

    Update addNews.php

--- addNews.php
+++ addNews.php
@@ -3,24 +3,25 @@
 <?php
     if(isset($_POST['formAddNews'])){
         if(!empty($_POST['titleAdd']) && !empty($_POST['contentAdd'])){
-            $titleAdd = $_POST['titleAdd'];
-            $contentAdd = $_POST['contentAdd'];
-            $dateTimeAdd = date('d/m/Y à H:i');
-            $insertNew = $db->prepare('INSERT INTO news(title, content, date_publication, id_user) VALUES(:title, :content, :date_publication, :id_user)');
-            $insertNew->execute(array(
-                'title'             => $titleAdd,
-                'content'           => $contentAdd,
-                'date_publication'  => $dateTimeAdd,
-                'id_user'           => $userInfo['id']
-            ));
+            $titleAdd = htmlspecialchars($_POST['titleAdd']);
+            if(strlen($titleAdd) <= 80) {
+                $contentAdd = nl2br(htmlspecialchars($_POST['contentAdd']));
+                $dateTimeAdd = date('d/m/Y à H:i');
+                $insertNew = $db->prepare('INSERT INTO news(title, content, date_publication, id_user) VALUES(:title, :content, :date_publication, :id_user)');
+                $insertNew->execute(array(
+                    'title'             => $titleAdd,
+                    'content'           => $contentAdd,
+                    'date_publication'  => $dateTimeAdd,
+                    'id_user'           => $userInfo['id']
+                ));
+            } else {
+                $error = "Le titre ne doit pas dépasser 80 caractères.";
+            }
         } else {
             $error = "Tous les champs doivent être renseignés.";
         }
     }
 ?>
-
-<script src="http://js.nicedit.com/nicEdit-latest.js" type="text/javascript"></script>
-<script type="text/javascript">bkLib.onDomLoaded(nicEditors.allTextAreas);</script>
 
 <?php 
     if(isset($userInfo['id'])){ 
@@ -32,14 +33,21 @@
     <div class="firstBlock">
         <form method="POST" action="">
             <input type="text" name="titleAdd" id="titleAdd" placeholder="Entrez le titre de l'article">
-            <textarea rows="10" name="contentAdd" style="width: 100%;"></textarea>
+            <textarea rows="10" name="contentAdd" style="width: 100%;"><?php if(isset($_POST['contentAdd'])) { echo $_POST['contentAdd']; } ?></textarea>
             <input type="submit" name="formAddNews" value="Publier l'article">
         </form>
+        <div class="error">
+            <?php if(isset($error)){
+                echo $error;
+            }
+            ?>
+        </div>
     </div>
     <!-- --------------------- -->
 
     <!-- CONTENU SECONDAIRE -->
     <div class="secondBlock">
+        - Le titre doit faire 80 caractères MAXIMUM !<br />
         - Attention aux fautes d'orthographes !<br />
         - Pas de propos racistes ou insultants. <br />
         - Pas de message haineux ...<br />
