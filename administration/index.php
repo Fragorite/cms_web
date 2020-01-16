8b6adce430a46acc689d174056ab374b067323c4
Fragorite <earvin.mars@gmail.com>
2020-01-16 15:26:55 +0100

    esfzseszge

--- administration/index.php
+++ administration/index.php
@@ -1,11 +1,21 @@
 <?php include('../includes/config.php'); // Configuration générale ?>
 
 <header class="headerMenu">
-<?php include('includes/menu.php'); ?>
-<div class="container">
-Bienvenu sur le panel administrateur <b><?= $userInfo['username']; ?>
-</div>
+    <?php include('includes/menu.php'); ?>
 
+    <div class="container">
+        <h1>Bienvenue sur le panel administrateur <div class="error"><?= $userInfo['username']; ?></div></h1>
+        <p>
+            Grâce à ce panel, vous pouvez :<br/>
+            - Changer le template principal,<br />
+            - Changer le nom de votre site,<br />
+            - Gérer les articles (news),<br />
+            - Gérer le chat (messages et utilisateurs),<br />
+            - Gérer le forum.<br />
+            <br />
+            <b>Bonne visite !</b>
+        </p>
+    </div>
 </header>
 
 
