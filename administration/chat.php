2a8eef8d94d9df76d4cb9d0d6ae4660c26d32245
Fragorite <earvin.mars@gmail.com>
2020-01-17 09:01:20 +0100

    Update chat.php

--- administration/chat.php
+++ administration/chat.php
@@ -75,7 +75,7 @@ $('#chat').load('chat.php #chat').fadeIn("slow");
                         <legend>
                             <b><?= $messages['date_publication']; ?></b>
                         </legend>
-                        <?php if($author['admin'] > 0) { echo '<b><div style="color: red; display: inline-block">[ADMIN] '.$author['username']; } ?> : <?= $messages['content']; ?></div></b> <a href="?delete=<?= $messages['id']; ?>"><i class="fas fa-times" style="color: red"></i></a>
+                        <?php if($author['admin'] > 0) { echo '<b><div style="color: red;">[ADMIN] '.$author['username'].'</div>'; } ?> : <?= $messages['content']; ?></b> <a href="?delete=<?= $messages['id']; ?>"><i class="fas fa-times" style="color: red"></i></a>
                     </fieldset>
                 </p>
             <?php
